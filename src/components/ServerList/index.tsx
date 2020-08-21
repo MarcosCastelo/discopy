8287e6ecfc81a56d5279a5181b045a1ef49d2963
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-21 12:53:45 -0300

    Adding basic components of server list

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ServerButton from '../ServerButton'
+
+import { Container, Separator } from './styles';
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+        <ServerButton isHome />
+
+        <Separator />
+
+        <ServerButton hasNotifications />
+        <ServerButton />
+        <ServerButton />
+        <ServerButton mentions={4}/>
+        <ServerButton />
+        <ServerButton />
+        <ServerButton />
+    </Container>
+  );
+}
+
+export default ServerList;
