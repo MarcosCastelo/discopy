c256fef33181692e64fea8ddc8892e796ff32a03
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-26 02:25:13 -0300

    Finishing the style of messages

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
-import ChannelMessage from '../ChannelMessage';
+import ChannelMessage, { Mention } from '../ChannelMessage';
 
 const ChannelData: React.FC = () => {
   return (
@@ -11,6 +11,79 @@ const ChannelData: React.FC = () => {
               author="Marcos Castelo"
               date = "02/07/2020"
               content = "Hoje é meu aniversário!"
+            />
+
+            <ChannelMessage 
+              author="Marcos Castelo"
+              date = "02/07/2020"
+              content = "Uma mensagem"
+            />
+
+            <ChannelMessage 
+              author="Marcos Castelo"
+              date = "02/07/2020"
+              content = "Uma mensagem"
+            />
+
+            <ChannelMessage 
+              author="Marcos Castelo"
+              date = "02/07/2020"
+              content = "Uma mensagem"
+            />
+
+            <ChannelMessage 
+              author="Marcos Castelo"
+              date = "02/07/2020"
+              content = "Uma mensagem"
+            />
+          
+            <ChannelMessage 
+              author="Marcos Castelo"
+              date = "02/07/2020"
+              content = "Hoje é meu aniversário!"
+            />
+
+
+            <ChannelMessage 
+              author="Marcos Castelo"
+              date = "02/07/2020"
+              content = "Uma mensagem"
+            />
+
+            <ChannelMessage 
+              author="Loritta"
+              date = "26/08/2020"
+              content = {
+                <> 
+                  <Mention>@Marcos Castelo</Mention>, vou dominar o mundo XD !!!
+                </>
+              }
+              hasMention
+              isBot
+            />
+
+            <ChannelMessage 
+              author="Marcos Castelo"
+              date = "02/07/2020"
+              content = "Uma mensagem"
+            />
+
+            <ChannelMessage 
+              author="Marcos Castelo"
+              date = "02/07/2020"
+              content = "Hoje é meu aniversário!"
+            />
+
+            <ChannelMessage 
+              author="Loritta"
+              date = "26/08/2020"
+              content = {
+                <> 
+                  <Mention>@Marcos Castelo</Mention>, vou dominar o mundo XD !!!
+                </>
+              }
+              hasMention
+              isBot
             />
           </Messages>
 
