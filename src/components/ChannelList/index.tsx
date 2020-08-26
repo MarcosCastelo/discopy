57de854dfc0ea0220b61a0c48b9d336002dd6242
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-25 23:31:50 -0300

    Adding items, styles and props to ChannelButton

--- src/components/ChannelList/index.tsx
+++ src/components/ChannelList/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
 import { Container, Category, AddCategoryIcon } from './styles';
+import ChannelButton from '../ChannelButton';
 
 const ChannelList: React.FC = () => {
   return (
-      <Container>
-          <Category> 
-            <span>Canais de texto</span>
-            <AddCategoryIcon />
-          </Category>
-      </Container>
+    <Container>
+      <Category>
+        <span>Canais de texto</span>
+        <AddCategoryIcon />
+      </Category>
+      <ChannelButton channelName="comunicados"/>
+      <ChannelButton channelName="geral"/>
+      <ChannelButton channelName="bots"/>
+      <ChannelButton channelName="promoções"/>
+      <ChannelButton channelName="nostalgia"/>
+      <ChannelButton channelName="flutter"/>
+    </Container>
   );
 }
 
