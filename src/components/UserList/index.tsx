42a2cecbc33069e99b485d2ac33c26a8f78ea3cb
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-26 00:44:22 -0300

    Creating the sctruture of UserList component

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -5,7 +5,11 @@ import { Container } from './styles';
 const UserList: React.FC = () => {
   return (
       <Container>
+        <Role>Disponível - 1</Role>
+        <UserRow nickname="Marcos Castelo" />
 
+        <Role>Offline - 18</Role>
+        <UserRow nickname="Loritta" isBot />
       </Container>
   );
 }
