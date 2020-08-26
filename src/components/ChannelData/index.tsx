30802f22f24876a7eab2db59b13a6fedae1a315a
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-26 02:35:39 -0300

    Using react hook to scroll the recent message

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,97 +1,74 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
 import ChannelMessage, { Mention } from '../ChannelMessage';
 
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef])
+
   return (
-      <Container>
-          <Messages>
-            <ChannelMessage 
-              author="Marcos Castelo"
-              date = "02/07/2020"
-              content = "Hoje é meu aniversário!"
-            />
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key = {n}
+            author = "Marcos Castelo"
+            date = "02/07/2020"
+            content = "Hoje é meu aniversário!"
+          />
 
-            <ChannelMessage 
-              author="Marcos Castelo"
-              date = "02/07/2020"
-              content = "Uma mensagem"
-            />
+        ))}
 
-            <ChannelMessage 
-              author="Marcos Castelo"
-              date = "02/07/2020"
-              content = "Uma mensagem"
-            />
-
-            <ChannelMessage 
-              author="Marcos Castelo"
-              date = "02/07/2020"
-              content = "Uma mensagem"
-            />
-
-            <ChannelMessage 
-              author="Marcos Castelo"
-              date = "02/07/2020"
-              content = "Uma mensagem"
-            />
-          
-            <ChannelMessage 
-              author="Marcos Castelo"
-              date = "02/07/2020"
-              content = "Hoje é meu aniversário!"
-            />
-
-
-            <ChannelMessage 
-              author="Marcos Castelo"
-              date = "02/07/2020"
-              content = "Uma mensagem"
-            />
-
-            <ChannelMessage 
-              author="Loritta"
-              date = "26/08/2020"
-              content = {
-                <> 
-                  <Mention>@Marcos Castelo</Mention>, vou dominar o mundo XD !!!
+        <ChannelMessage
+          author="Loritta"
+          date="26/08/2020"
+          content={
+            <>
+              <Mention>@Marcos Castelo</Mention>, vou dominar o mundo XD !!!
                 </>
-              }
-              hasMention
-              isBot
-            />
+          }
+          hasMention
+          isBot
+        />
 
-            <ChannelMessage 
-              author="Marcos Castelo"
-              date = "02/07/2020"
-              content = "Uma mensagem"
-            />
+        <ChannelMessage
+          author="Marcos Castelo"
+          date="02/07/2020"
+          content="Uma mensagem"
+        />
 
-            <ChannelMessage 
-              author="Marcos Castelo"
-              date = "02/07/2020"
-              content = "Hoje é meu aniversário!"
-            />
+        <ChannelMessage
+          author="Marcos Castelo"
+          date="02/07/2020"
+          content="Hoje é meu aniversário!"
+        />
 
-            <ChannelMessage 
-              author="Loritta"
-              date = "26/08/2020"
-              content = {
-                <> 
-                  <Mention>@Marcos Castelo</Mention>, vou dominar o mundo XD !!!
+        <ChannelMessage
+          author="Loritta"
+          date="26/08/2020"
+          content={
+            <>
+              <Mention>@Marcos Castelo</Mention>, vou dominar o mundo XD !!!
                 </>
-              }
-              hasMention
-              isBot
-            />
-          </Messages>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
 
-          <InputWrapper>
-            <Input type="text" placeholder="Conversar em #geral"/>
-            <InputIcon />
-          </InputWrapper>
-      </Container>
+      <InputWrapper>
+        <Input type="text" placeholder="Conversar em #geral" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
   );
 }
 
