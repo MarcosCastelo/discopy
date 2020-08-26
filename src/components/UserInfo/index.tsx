78a1767c1165091a1b94da5a2d2149062e2fc35b
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-25 23:55:09 -0300

    Stylizing user info items

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { 
+    Container, 
+    Profile, 
+    Avatar, 
+    UserData, 
+    Icons, 
+    MicIcon, 
+    HeadphoneIcon, 
+    SettingsIcon
+} from './styles';
 
 const UserInfo: React.FC = () => {
     return (
         <Container>
-
+            <Profile>
+                <Avatar />
+                <UserData>
+                    <strong>Marcos Castelo</strong>
+                    <span>#2684</span>
+                </UserData>
+            </Profile>
+            <Icons>
+                <MicIcon />
+                <HeadphoneIcon />
+                <SettingsIcon />
+            </Icons>
         </Container>
     );
 }
